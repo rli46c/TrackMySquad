77a3152dfd1fea74ec2309bf04fd778133749a2d
rajan <webdev.chd@gmail.com>
2020-02-22 18:07:11 +0530

    setting of hame page

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ if (localStorage.token) {
 }
 
 const App = () => {
-
   useEffect(() => {
     store.dispatch(loadUser());
   }, []);
--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import {
+  makeStyles,
+  CssBaseline,
+  Grid,
+  Paper,
+  Container
+} from '@material-ui/core';
+import { ArchiveRounded } from '@material-ui/icons';
 
 import Appbar from '../layout/Appbar';
 
-export const HomePage = ({}) => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: 'red'
+  }
+}));
 
-    return (
-        <div>
-            
-        </div>
-    );
+const HomePage = ({}) => {
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <CssBaseline />
+      <Appbar />
+      <Container component='main' maxWidth='md' className={classes.root}>
+        <Grid container spacing={5} alignItems='flex-end'>
+          <Grid xs={12} md={10}>
+            This is Home page
+          </Grid>
+        </Grid>
+      </Container>
+    </Fragment>
+  );
 };
 
 HomePage.propTypes = {
-    // prop: PropTypes
-}
-
-const mapStateToProps = (state) => ({
-    
-});
-
-const mapDispatchToProps = {
-    
+  // prop: PropTypes
 };
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
