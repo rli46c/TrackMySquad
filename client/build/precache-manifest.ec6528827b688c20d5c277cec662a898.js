662e3b0bc0bcd338f4fcea16ab7756796356c3ba
Vinayak Sharma <vinayak252@gmail.com>
2020-03-15 13:38:32 +0530

    Build script run for checking whether deployment ready

---
+++ client/build/precache-manifest.ec6528827b688c20d5c277cec662a898.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abaf5a5773098708b6f0a270076c29e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05d6a5cf62dec2080f71",
+    "url": "/static/css/main.08929a35.chunk.css"
+  },
+  {
+    "revision": "595d8f5cae16aa2c7d31",
+    "url": "/static/js/2.147d0781.chunk.js"
+  },
+  {
+    "revision": "a5b0d3ec22852c796318b06a3348f0fa",
+    "url": "/static/js/2.147d0781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d6a5cf62dec2080f71",
+    "url": "/static/js/main.71fcb5fd.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "f9faaf59e42311f2fcd1733a39d9b00a",
+    "url": "/static/media/bannertime.f9faaf59.jpg"
+  },
+  {
+    "revision": "af12abc0f8c6c205ab20514627e09dfd",
+    "url": "/static/media/clipart-maleuser.af12abc0.png"
+  },
+  {
+    "revision": "8023d85dfd28a5230fa440977d68c064",
+    "url": "/static/media/companylogo.8023d85d.png"
+  },
+  {
+    "revision": "702917ccc0f69ad43bc0f01dcf1bf2e1",
+    "url": "/static/media/reports.702917cc.png"
+  },
+  {
+    "revision": "c5be2df953d82f876648cb0318a6a2bb",
+    "url": "/static/media/sm-app.c5be2df9.png"
+  },
+  {
+    "revision": "427d030c6017b93a5f39089c7765de31",
+    "url": "/static/media/sm-shot.427d030c.png"
+  },
+  {
+    "revision": "0234036ff2c196ebaa96e5bf5f25ea4e",
+    "url": "/static/media/sm-time.0234036f.png"
+  },
+  {
+    "revision": "7aaa99fca67ca220b4c951deea3c4a08",
+    "url": "/static/media/timescreen.7aaa99fc.png"
+  },
+  {
+    "revision": "f59a893d82356e2e2452e8201fb072d9",
+    "url": "/static/media/timetracker.f59a893d.png"
+  },
+  {
+    "revision": "a383c63ba35f1c2b66253fce6e60b4d0",
+    "url": "/static/media/tracktime.a383c63b.png"
+  }
+]);
